11275fdf5e2dded6884d99e33daa0ef506cfa0a6
wurfkreuz <wurfkreuz@mail.ru>
2023-08-17 17:27:56 +0300

    new

--- src/postgres_cli.nim
+++ src/postgres_cli.nim
@@ -1,19 +1,45 @@
-import db_connector/db_postgres, #[ os, ]# #[ strutils, ]# parseopt
+import db_connector/db_postgres, parseopt, std/terminal
 
+var
+  database = ""
+  user = ""
+  password = readPasswordFromStdin("Enter the password: ")
+  activeFlag = false
+  argCount = 0
+
+let getActiveDatabases = sql"SELECT DISTINCT datname FROM pg_stat_activity;"
 
 # parse command-line arguments
 var p = initOptParser()  # This sets up a parser to handle command-line arguments.
 for kind, key, val in p.getopt():
-  if kind == cmdArgument:  
-    # connect to the database
-    let conn = open(key, "user", "password", "localhost")
+  if kind == cmdArgument and user == "":  # cmdArgument is used for arguments that are attached to an option, like botdb in -d botdb. Positional arguments like bot in your command line input are classified as cmdEnd. 
+    user = key
+    # inc argCount
+  elif kind == cmdShortOption and key == "d":
+    database = val
+    # inc argCount
+  elif kind == cmdLongOption and key == "active":
+    activeFlag = true
+  inc argCount
 
-    # execute SQL query
+# echo "User: ", user
+# echo "Database: ", database
+
+if database != "" and user != "":
+  let conn = open("localhost", user, password, database)
+
+  if activeFlag:
+    for row in conn.fastRows(getActiveDatabases):
+      echo row[0]
+
+  if argCount == 2:
     for row in conn.fastRows(sql"SELECT tablename FROM pg_tables WHERE schemaname = 'public';"):
       echo row[0]
 
-    # close the connection
-    conn.close()
+  # close the connection
+  conn.close()
+else:
+  echo "Missing necessary information to connect to the database."
 
 
 # cmdArgument represents a command-line argument (e.g., a standalone value 
