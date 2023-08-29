06b007e96fdcdd77799e8179a0fa97fa5d6ed97c
wurfkreuz <wurfkreuz@mail.ru>
2023-08-29 20:50:24 +0300

    n

--- src/postgres_cli.nim
+++ src/postgres_cli.nim
@@ -1,10 +1,11 @@
-import db_connector/db_postgres, parseopt, std/terminal
+import db_connector/db_postgres, parseopt, std/terminal, strutils, os, osproc
 
 var
   database = ""
   user = ""
   password = readPasswordFromStdin("Enter the password: ")
   activeFlag = false
+  replaceFlag = false
   argCount = 0
 
 let getActiveDatabases = sql"SELECT DISTINCT datname FROM pg_stat_activity;"
@@ -20,6 +21,8 @@ for kind, key, val in p.getopt():
     # inc argCount
   elif kind == cmdLongOption and key == "active":
     activeFlag = true
+  elif kind == cmdLongOption and key == "replace":
+    replaceFlag = true
   inc argCount
 
 # echo "User: ", user
@@ -32,8 +35,26 @@ if database != "" and user != "":
     for row in conn.fastRows(getActiveDatabases):
       echo row[0]
 
+  if replaceFlag:
+    echo "Enter the name of the table you want to replace:"
+    let oldTable = readLine(stdin)
+    echo "Enter path to the new table:"
+    let sqlFilePath = readLine(stdin)
+
+    if fileExists(sqlFilePath):
+      try:
+        conn.exec(sql"DROP TABLE ", oldTable)
+        let fileContent = readFile(sqlFilePath)
+        let commands = fileContent.split(";")
+        for command in commands:
+          if command.strip() != "":
+            conn.exec(sql(command))
+        conn.close()
+      except:
+        echo "An error occurred while replacing the table."
+  
   if argCount == 2:
-    for row in conn.fastRows(sql"SELECT tablename FROM pg_tables WHERE schemaname = 'public';"):
+    for row in conn.fastRows(sql"SELECT tablename FROM pg_tables;"):
       echo row[0]
 
   # close the connection
@@ -57,3 +78,5 @@ else:
 # key: This represents the actual option key or argument. For short and long options, this would be the name of the option (without the dashes). For a non-option argument, this would be the argument itself.
 #
 # val: This represents the value associated with an option if it has one. If an option has a value (e.g., --option=value), then val will contain that value. If the option does not have a value, val will be an empty string.
+
+# i can't use let to create the 'success' varible because it can throw an exception.
